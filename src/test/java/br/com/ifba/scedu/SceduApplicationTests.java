b5ab174878f7136bd371381c4e7054de0658db1c
Pormiiles <andrelima.santos2370@gmail.com>
2024-08-14 11:42:17 -0300

    First commit

---
+++ src/test/java/br/com/ifba/scedu/SceduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.scedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SceduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
