0b54ea9f78e562c890be007a72405706af7df482
Matheus Mendes <msmmendes211@gmail.com>
2024-08-25 14:12:17 -0300

    feat: adiciona DTO

---
+++ src/main/java/br/com/ifba/scedu/domain/entities/professor/dto/ProfessorResponseDTO.java
@@ -0,0 +1,28 @@
+package br.com.ifba.scedu.domain.entities.professor.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfessorResponseDTO {
+
+        @JsonProperty(value = "id")
+        private Long id;
+
+        @JsonProperty(value = "name")
+        private String name;
+
+        @JsonProperty(value = "siape")
+        private String siape;
+
+        @JsonProperty(value = "departamento")
+        private String departamento;
+
+        @JsonProperty(value = "cargaHoraria")
+        private Integer cargaHoraria;
+
+}
