8d388631b8584896cc31542e63094246b06d42d6
Matheus Mendes <msmmendes211@gmail.com>
2024-08-25 03:03:24 -0300

    remove: remove metodo não utilizado do repository

--- src/main/java/br/com/ifba/scedu/domain/entities/professor/repository/ProfessorRepository.java
+++ src/main/java/br/com/ifba/scedu/domain/entities/professor/repository/ProfessorRepository.java
@@ -2,11 +2,10 @@ package br.com.ifba.scedu.domain.entities.professor.repository;
 
 import br.com.ifba.scedu.domain.entities.professor.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface ProfessorRepository  extends JpaRepository<Professor, Long> {
 
-    Optional<Professor> findByEmail(String email);
     boolean existsByEmail(String email);
 }
