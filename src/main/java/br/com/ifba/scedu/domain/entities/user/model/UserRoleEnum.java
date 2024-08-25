0a23c1e2140afcb839493b6d63ec94265932a0ed
Matheus Mendes <msmmendes211@gmail.com>
2024-08-25 02:47:40 -0300

    fix: utilização da anotação @Getter em UserRoleEnum

--- src/main/java/br/com/ifba/scedu/domain/entities/user/model/UserRoleEnum.java
+++ src/main/java/br/com/ifba/scedu/domain/entities/user/model/UserRoleEnum.java
@@ -1,16 +1,16 @@
 package br.com.ifba.scedu.domain.entities.user.model;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRoleEnum {
     ADMIN("admin"),
     USER("user");
 
-    private String role;
+    private final String role;
 
     UserRoleEnum(String role){
         this.role = role;
     }
 
-    public String getRole(){
-        return role;
-    }
 }
